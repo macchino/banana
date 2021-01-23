84633b98e89cc0b34460c446697b3d146c9c4dda
macchino <mac.kimitoshi@gmail.com>
2021-01-23 11:08:27 +0900

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -187,10 +187,10 @@ OSCAR_CURRENCY_FORMAT = '¤#,##0'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-# STATIC_URL = '/static/'
-# STATICFILES_DIRS =[
-#     os.path.join(BASE_DIR, 'static')
-# ]
+STATIC_URL = '/static/'
+STATICFILES_DIRS =[
+    os.path.join(BASE_DIR, 'static')
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
