fe79e3fa5450134f2e5c31c8e4f7b8b17fddfa78
macchino <mac.kimitoshi@gmail.com>
2021-02-09 14:47:34 +0900

    Add EmailBackend

--- mysite/settings.py
+++ mysite/settings.py
@@ -169,6 +169,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
